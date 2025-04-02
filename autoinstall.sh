49865c5afd517c96e46df12678be33345abcc000
hachiko008 <joshuagalang8@gmail.com>
2025-04-02 16:46:34 +0800

    fixes
    dami neto hahaha sakit sa ulo lmao

--- autoinstall.sh
+++ autoinstall.sh
@@ -11,10 +11,11 @@ display_menu() {
     echo "2. Install Postgresql 9.6"
     echo "3. Install Java 7"
     echo "4. Install Glassfish 4"
-    echo "5. Install Jasper 6"
-    echo "6. Create Autostart Glassfish"
-    echo "7. Create Autostart Jasper"
-    echo "8. Exit"
+    echo "5. Install JDBC Connector [REQUIRED] [ALPHA]"
+    echo "6. Install Jasper 6"
+    echo "7. Create Autostart Glassfish"
+    echo "8. Create Autostart Jasper"
+    echo "9. Exit"
     echo "*******************************************************************"
 }
 
@@ -40,8 +41,18 @@ install_prerequisites() {
 }
 install_postgresql() {
 if sudo wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - &&
-    echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql-pgdg.list > /dev/null &&
-    #echo "deb https://apt-archive.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql-pgdg.list > /dev/null &&
+    UBUNTU_VERSION=$(lsb_release -rs | cut -d'.' -f1)
+
+    if [ "$UBUNTU_VERSION" -ge 18 ]; then
+        echo "deb https://apt-archive.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | \
+        sudo tee /etc/apt/sources.list.d/postgresql-pgdg.list > /dev/null &&
+        echo "$UBUNTU_VERSION detected! I'm gonna use the repo with https:// proxy."
+    else
+        echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | \
+        sudo tee /etc/apt/sources.list.d/postgresql-pgdg.list > /dev/null &&
+        echo "$UBUNTU_VERSION detected! I'm gonna use the repo with http:// proxy."
+    fi
+
     sudo apt update &&
     sudo apt install postgresql-9.6 -y &&
     sudo sed -i 's/#listen_addresses = '\''localhost'\''/listen_addresses = '\''*'\''/g' /etc/postgresql/9.6/main/postgresql.conf &&
@@ -64,7 +75,7 @@ else
 fi
 }
 install_java() {
-    sudo wget -P /home/xmanager/ https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/jdk-7u80-linux-x64.tar.gz || { printf "Failed to download Java JDK"; return 1; }
+    sudo wget -P ~/ https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/jdk-7u80-linux-x64.tar.gz || { printf "Failed to download Java JDK"; return 1; }
 
     if sudo mkdir -p /usr/local/java &&
         sudo mv jdk-7u80-linux-x64.tar.gz /usr/local/java/ &&
@@ -89,8 +100,8 @@ install_java() {
 
 install_glassfish() {
        printf "Commencing Glassfish 4 Installation... \n" &&
-    if sudo wget -P /home/xmanager https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/glassfish-4.1.2.zip &&
-        sudo unzip /home/xmanager/glassfish-4.1.2.zip &&
+    if  sudo wget -P ~ https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/glassfish-4.1.2.zip &&
+        sudo unzip ~/glassfish-4.1.2.zip &&
         sudo ~/glassfish4/glassfish/bin/asadmin start-domain &&
         printf "Change admin password manually\n" &&
         sudo ~/glassfish4/glassfish/bin/asadmin change-admin-password &&
@@ -100,11 +111,8 @@ install_glassfish() {
         sudo ~/glassfish4/glassfish/bin/asadmin restart-domain &&
         printf "Creating JDBC Pool...\n" &&
         sudo ~/glassfish4/glassfish/bin/asadmin create-jdbc-connection-pool --datasourceclassname org.postgresql.ds.PGConnectionPoolDataSource --restype javax.sql.ConnectionPoolDataSource --property portNumber=7477:databaseName=icbs:serverName=127.0.0.1:user=postgres:password=postgres icbs &&
-        printf "Installing postgresql connector... \n" &&
-        sudo wget -P /home/xmanager/glassfish4/domains/domain1/lib https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/postgresql-9.3-1103.jdbc4.jar &&
-        sudo unzip /home/xmanager/glassfish4/domains/domain1/lib/postgresql-9.3-1103.jdbc4.jar &&
         printf "Changing Perm size... \n" &&
-        sudo sed -i 's/-XX:MaxPermSize=192m/-XX:MaxPermSize=8196m/; s/-Xmx512m/-Xmx8196m/' ~/glassfish4/domains/domain1/config/domain.xml &&
+        sudo sed -i 's/-XX:MaxPermSize=192m/-XX:MaxPermSize=8196m/; s/-Xmx512m/-Xmx8196m/' ~/glassfish4/glassfish/domains/domain1/config/domain.xml &&
         #commenting these parts, pang https ssl lang to
         #printf "Https listener to 443... \n" &&
         #sudo sed -i 's/<network-listener port="8181"/<network-listener port="443"/g' ~/glassfish4/glassfish/domains/domain1/config/domain.xml
@@ -119,6 +127,12 @@ install_glassfish() {
         echo "Glassfish setup FAILED"
         return 1
     fi
+}
+
+install_jdbc_connector() {
+        printf "Installing postgresql connector... \n" &&
+        sudo wget -P ~/glassfish4/domains/domain1/lib https://github.com/mbphils/ICBS-Installer/releases/download/Required-Files/postgresql-9.3-1103.jdbc4.jar &&
+        sudo unzip ~/glassfish4/domains/domain1/lib/postgresql-9.3-1103.jdbc4.jar 
 }
 
 install_jasper() {
@@ -137,104 +151,155 @@ install_jasper() {
 }
 
 create_autostart_glassfish() {
-    if [ -f "/etc/init.d/glassfish" ]; then
-        printf "\nAutostart Glassfish detected! Deleting....\n"
-        sudo rm /etc/init.d/glassfish
-        printf "Success!\n\n"
-    fi
+        if [ -f "/etc/init.d/glassfish" ]; then
+            printf "\nAutostart Glassfish detected! Deleting....\n"
+            sudo rm /etc/init.d/glassfish
+            printf "Success!\n\n"
+        fi
 
-    if printf "Creating Autostart Glassfish...\n" &&
-        sudo sh -c 'cat > /etc/init.d/glassfish << EOF
-#!/bin/sh
-export AS_JAVA=/usr/local/java/jdk1.7.0_80
-GLASSFISHPATH="/home/xmanager/glassfish4/glassfish"
-case "\$1" in
-    start)
-        echo "Starting GlassFish from \$GLASSFISHPATH"
-        sudo $GLASSFISHPATH/bin/asadmin start-domain
-        ;;
-    restart)
-        $0 stop
-        $0 start
-        ;;
-    stop)
-        echo "Stopping GlassFish from \$GLASSFISHPATH"
-        sudo $GLASSFISHPATH/bin/asadmin stop-domain
-        ;;
-    *)
-        echo "Usage: \$0 {start|stop|restart}"
-        exit 1
-        ;;
-esac
+        if printf "Creating Autostart Glassfish...\n" &&
+            sudo sh -c 'cat > /etc/init.d/glassfish << EOF
+    #!/bin/sh
+    ### BEGIN INIT INFO
+    # Provides:          glassfish
+    # Required-Start:    $remote_fs $syslog
+    # Required-Stop:     $remote_fs $syslog
+    # Default-Start:     2 3 4 5
+    # Default-Stop:      0 1 6
+    # Short-Description: GlassFish Server
+    # Description:       Start and stop the GlassFish Server
+    ### END INIT INFO
 
-exit 0
-EOF'; then
-        sudo chmod +x /etc/init.d/glassfish &&
-        sudo update-rc.d glassfish defaults &&
-        sudo systemctl daemon-reload &&
-        sudo service glassfish restart &&
-        printf "\n\n"
-        #sudo service glassfish status &&
-        printf "Autostart Glassfish created successfully.\n\n" &&
-        return 0
-    else
-        printf "Creating Autostart Glassfish Failed \n"
-        return 1
-    fi
-}
-
-create_autostart_jasper() {
-    if [ -f "/etc/init.d/jasperserver" ]; then
-        printf "\nAutostart jasperserver detected! Deleting....\n"
-        sudo rm /etc/init.d/jasperserver
-        printf "Success!\n\n"
-    fi
-
-    if printf "\n Creating Autostart Jasper...\n\n" &&
-        sudo sh -c 'cat > /etc/init.d/jasperserver << "EOF"
-#!/bin/sh
-    JASPER_HOME="/opt/jasperreports-server-cp-6.3.0"
-    case "$1" in
+    export AS_JAVA=/usr/local/java/jdk1.7.0_80
+    GLASSFISHPATH="~/glassfish4/glassfish"
+    case "\$1" in
         start)
-            if [ -f "$JASPER_HOME/ctlscript.sh" ]; then
-                echo "Starting JasperServer"
-                "$JASPER_HOME/ctlscript.sh" start
-            fi
-            ;;
-        stop)
-            if [ -f "$JASPER_HOME/ctlscript.sh" ]; then
-                echo "Stopping JasperServer"
-                "$JASPER_HOME/ctlscript.sh" stop
-            fi
+            echo "Starting GlassFish from \$GLASSFISHPATH"
+            sudo $GLASSFISHPATH/bin/asadmin start-domain
             ;;
         restart)
-            if [ -f "$JASPER_HOME/ctlscript.sh" ]; then
-                echo "Restarting JasperServer"
-                "$JASPER_HOME/ctlscript.sh" restart
-            fi
+            $0 stop
+            $0 start
             ;;
-        status)
-            if [ -f "$JASPER_HOME/ctlscript.sh" ]; then
-                echo "Checking JasperServer status"
-                "$JASPER_HOME/ctlscript.sh" status
-            fi
+        stop)
+            echo "Stopping GlassFish from \$GLASSFISHPATH"
+            sudo $GLASSFISHPATH/bin/asadmin stop-domain
             ;;
         *)
-            echo $"Usage: $0 {start|stop|restart|status}"
+            echo "Usage: \$0 {start|stop|restart}"
             exit 1
             ;;
     esac
-EOF'; then
-        sudo chmod +x /etc/init.d/jasperserver &&
-        sudo update-rc.d jasperserver defaults &&
-        sudo systemctl daemon-reload &&
-        sudo service jasperserver restart &&
-        printf "Autostart JasperServer created successfully.\n\n" &&
-        return 0
-    else
-        printf "Creating Autostart JasperServer Failed \n" &&
-        return 1
-    fi
+
+    exit 0
+    EOF'; then
+            echo "[Unit]
+            Description=GlassFish Server
+            After=network.target
+
+            [Service]
+            Type=forking
+            ExecStart=/etc/init.d/glassfish start
+            ExecStop=/etc/init.d/glassfish stop
+            RemainAfterExit=yes
+
+            [Install]
+            WantedBy=multi-user.target" | sudo tee /etc/systemd/system/glassfish.service > /dev/null && \
+
+            # Reload systemd and enable the service
+            sudo chmod +x /etc/init.d/glassfish && \
+            sudo systemctl daemon-reload && \
+            sudo systemctl enable glassfish && \
+            sudo systemctl restart glassfish
+
+            printf "\n\n"
+            #sudo service glassfish status &&
+            printf "Autostart Glassfish created successfully.\n\n" &&
+            return 0
+        else
+            printf "Creating Autostart Glassfish Failed \n"
+            return 1
+        fi
+}
+
+    create_autostart_jasper() {
+        if [ -f "/etc/init.d/jasperserver" ]; then
+            printf "\nAutostart jasperserver detected! Deleting....\n"
+            sudo rm /etc/init.d/jasperserver
+            printf "Success!\n\n"
+        fi
+
+        if printf "\nCreating Autostart Jasper...\n\n" &&
+            sudo sh -c 'cat > /etc/init.d/jasperserver << "EOF"
+    #!/bin/sh
+    ### BEGIN INIT INFO
+    # Provides:          jasperserver
+    # Required-Start:    $remote_fs $syslog
+    # Required-Stop:     $remote_fs $syslog
+    # Default-Start:     2 3 4 5
+    # Default-Stop:      0 1 6
+    # Short-Description: JasperServer Service
+    # Description:       Start and stop JasperReports Server
+    ### END INIT INFO
+
+    JASPER_HOME="/opt/jasperreports-server-cp-6.3.0"
+
+    case "\$1" in
+        start)
+            if [ -f "\$JASPER_HOME/ctlscript.sh" ]; then
+                echo "Starting JasperServer"
+                "\$JASPER_HOME/ctlscript.sh" start
+            fi
+            ;;
+        stop)
+            if [ -f "\$JASPER_HOME/ctlscript.sh" ]; then
+                echo "Stopping JasperServer"
+                "\$JASPER_HOME/ctlscript.sh" stop
+            fi
+            ;;
+        restart)
+            if [ -f "\$JASPER_HOME/ctlscript.sh" ]; then
+                echo "Restarting JasperServer"
+                "\$JASPER_HOME/ctlscript.sh" restart
+            fi
+            ;;
+        status)
+            if [ -f "\$JASPER_HOME/ctlscript.sh" ]; then
+                echo "Checking JasperServer status"
+                "\$JASPER_HOME/ctlscript.sh" status
+            fi
+            ;;
+        *)
+            echo "Usage: \$0 {start|stop|restart|status}"
+            exit 1
+            ;;
+    esac
+
+    exit 0
+    EOF'; then
+            sudo chmod +x /etc/init.d/jasperserver &&
+            echo "[Unit]
+    Description=JasperReports Server
+    After=network.target
+
+    [Service]
+    Type=forking
+    ExecStart=/etc/init.d/jasperserver start
+    ExecStop=/etc/init.d/jasperserver stop
+    RemainAfterExit=yes
+
+    [Install]
+    WantedBy=multi-user.target" | sudo tee /etc/systemd/system/jasperserver.service > /dev/null && \
+
+            sudo systemctl daemon-reload && \
+            sudo systemctl enable jasperserver && \
+            sudo systemctl restart jasperserver && \
+            printf "Autostart JasperServer created successfully.\n\n" && \
+            return 0
+        else
+            printf "Creating Autostart JasperServer Failed\n" && \
+            return 1
+        fi
 }
 
 exit_script() {
@@ -252,11 +317,12 @@ main() {
             2) install_postgresql ;;
             3) install_java ;;
             4) install_glassfish ;;
-            5) install_jasper ;;
-            6) create_autostart_glassfish ;;
-            7) create_autostart_jasper ;;
-            8) exit_script ;;
-            *) printf "Invalid choice. Please choose a number between 1 and 8.\n" ;;
+            5) install_jdbc_connector ;;
+            6) install_jasper ;;
+            7) create_autostart_glassfish ;;
+            8) create_autostart_jasper ;;
+            9) exit_script ;;
+            *) printf "Invalid choice. Please choose a number between 1 and 9.\n" ;;
         esac
     done
 }
